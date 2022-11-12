c1f72375d0f214ec83d38177e76a00c40476220f
nadiazafirazahra <114532973+nadiazafirazahra@users.noreply.github.com>
2022-11-12 18:22:55 +0700

    Update SegiTiga.java

--- SegiTiga.java
+++ SegiTiga.java
@@ -1,12 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PBONadiaZafiraZahra;
 
 /**
- *
+ * Nama: Nadia Zafira Zahra
+ * NIM: 20210040174
+ * Kelas: TI21F
  * @author Acer
  */
 public class SegiTiga extends BangunDatar  {
